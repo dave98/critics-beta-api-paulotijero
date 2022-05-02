5b99d352d14674eb514b35eaad14d2cc47c50b46
dave98 <jose.mamani.vilca@ucsp.edu.pe>
2022-05-02 12:28:04 -0500

    Adding some useful comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referer || root_path)
+    redirect_to(request.referer || root_path)  # request.referer -> Lo redirecciono al lugar de donde viene || sino al root_path
   end
 
   protected
--- app/policies/critic_policy.rb
+++ app/policies/critic_policy.rb
@@ -1,12 +1,12 @@
 class CriticPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      return scope.where(approved: true) unless user
+      return scope.where(approved: true) unless user # current_user // nil
 
       if user.admin?
         scope.all
       else
-        scope.where(user: user).or(scope.where(approved: true))
+        scope.where(user: user).or(scope.where(approved: true)) # Critic.where(user: user).or(Critic.where(approved: true)) -> No es un or de lógica sino a nivel de sql.
       end
     end
   end
@@ -36,6 +36,6 @@ class CriticPolicy < ApplicationPolicy
   end
 
   def destroy?
-    user.admin? || record.user == user
+    user.admin? || record.user == user # Equivalente a: current_user.admin? ||  @Critic.user = ...
   end
 end
